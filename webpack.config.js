522035bf07d945c40d6a1fbc52f85f2696003d65
moxuse <moxuse@gmail.com>
2017-05-24 13:36:29 +0900

    Production build setting

--- webpack.config.js
+++ webpack.config.js
@@ -87,7 +87,7 @@ module.exports = {
   plugins: [
     new webpack.DefinePlugin({
       'process.env': {
-        NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+        NODE_ENV: JSON.stringify('production')
       }
     }),
     new ExtractTextPlugin("[name].css"),
