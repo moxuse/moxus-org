a4e287107c350d0af7f8524148ada3797aa8b092
moxuse <moxuse@gmail.com>
2019-06-04 17:08:23 +0900

    monokai sublime

--- src/javascripts/components/container.jsx
+++ src/javascripts/components/container.jsx
@@ -27,7 +27,7 @@ class Container extends Component {
       new PostLoader(item.path)
         .then((res) => {
           const key = res.attributes.post_id + Date.now() + Math.random();
-          const contents = <Contents 
+          const contents = <Contents
             title={res.attributes.title} 
             date={date} 
             body={res.body} 
