ea02484cf5a64a87f4884bdfee53bee8c2fc6cef
moxuse <moxuse@gmail.com>
2019-11-08 17:38:28 +0900

    fix webpack.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,22 @@
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-const PRODUCTION = process.env.NODE_ENV === 'production';
+const PRODUCTION = process.env.NODE_ENV === "production";
 
 module.exports = {
   entry: {
-    app: './src/javascripts/app.jsx'
+    app: "./src/javascripts/app.jsx"
   },
   output: {
-    path: __dirname + '/public',
-    filename: 'javascripts/[name]-[hash:8].js',
-    publicPath: '/'
+    path: __dirname + "/public",
+    filename: "javascripts/[name]-[hash:8].js",
+    publicPath: "/"
   },
   resolve: {
     alias: {
-      normalize: __dirname + '/node_modules/normalize.css/'
+      normalize: __dirname + "/node_modules/normalize.css/"
     }
   },
   module: {
@@ -24,106 +24,90 @@ module.exports = {
       {
         test: /\.html$/,
         exclude: /node_modules/,
-        loader: 'html-loader',
+        loader: "html-loader"
       },
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
+        loader: "babel-loader",
         query: {
-          presets: [
-            ['env', { 'modules': false }],
-            'react',
-            'es2015'
-          ]
+          presets: [["env", { modules: false }], "react", "es2015"]
         }
       },
       {
         test: /\.css$/,
         use: [
-          {loader: 'style-loader'},
+          { loader: "style-loader" },
           {
-            loader: 'css-loader',
+            loader: "css-loader",
             options: {
-              modules: true,
-              localIdentName: '[name]__[local]--[hash:base64:5]',
-              includePaths: ['node_modules/flexboxgrid']
+              modules: {
+                localIdentName: "[name]__[local]--[hash:base64:5]"
+                // includePaths: ["node_modules/flexboxgrid"]
+              }
             }
           }
         ]
       },
       {
         test: /\.jpe?g$|\.gif$|\.png$/i,
-        loader: 'file-loader'
+        loader: "file-loader"
       },
       {
         test: /\.(glsl|frag|vert)$/,
         exclude: /node_modules/,
-        loaders: ['raw-loader', 'glslify-loader']
+        loaders: ["raw-loader", "glslify-loader"]
       },
       {
-        type: 'javascript/auto',
+        type: "javascript/auto",
         test: /\.json$/,
-        use: [ { loader: 'json-loader' } ]
+        use: [{ loader: "json-loader" }]
       },
       {
         test: /\.yaml$/,
-        use: [
-          'file-loader',
-          'json-loader',
-          'yaml-loader'
-        ]
+        use: ["file-loader", "json-loader", "yaml-loader"]
       },
       {
         test: /\.scss$/,
         loader: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: 'css-loader!sass-loader'
+          fallback: "style-loader",
+          use: "css-loader!sass-loader"
         })
       },
       {
         test: /\.md$/,
-        use: [
-          'markdown-with-front-matter-loader'
-        ]
+        use: ["markdown-with-front-matter-loader"]
       }
     ]
   },
 
   node: {
     console: true,
-    fs: 'empty'
+    fs: "empty"
   },
 
-  devtool: PRODUCTION ? false : 'inline-source-map',
+  devtool: PRODUCTION ? false : "inline-source-map",
 
   plugins: [
     new webpack.ExtendedAPIPlugin(),
-    new ExtractTextPlugin('[name].css'),
+    new ExtractTextPlugin("[name].css"),
     new HtmlWebpackPlugin({
-      inject: 'body',
-      template: './src/html/index.html'
+      inject: "body",
+      template: "./src/html/index.html"
     }),
     new CopyWebpackPlugin(
-      [
-        { from: './src/', to: './', ignore: ['index.html'] }
-      ],
+      [{ from: "./src/", to: "./", ignore: ["index.html"] }],
       {
-        ignore:
-        [
-          '.DS_Store',
-          '.gitkeep',
-          'javascripts/components/**/*',
-          'javascripts/lib/*',
-          'stylesheets/sass/*',
-          'javascripts/app.jsx'
+        ignore: [
+          ".DS_Store",
+          ".gitkeep",
+          "javascripts/components/**/*",
+          "javascripts/lib/*",
+          "stylesheets/sass/*",
+          "javascripts/app.jsx"
         ]
       }
     ),
-    ...(
-      PRODUCTION ? [
-        new webpack.optimize.OccurrenceOrderPlugin()
-      ] : []
-    )
+    ...(PRODUCTION ? [new webpack.optimize.OccurrenceOrderPlugin()] : [])
   ]
 };
