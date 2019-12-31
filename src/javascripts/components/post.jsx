c27342c2ec78e7ae1270365d86c18ecca5357864
moxuse <moxuse@gmail.com>
2019-12-31 22:07:52 +0900

    added new post

--- src/javascripts/components/post.jsx
+++ src/javascripts/components/post.jsx
@@ -6,7 +6,14 @@ import React, { Component } from 'react';
 
 import Container from './container.jsx';
 
+import loadTwitterWidgets from '../lib/LoadTwitterWidgets.js'
+
 class Post extends Component {
+  componentDidMount() {
+    // for twitter widget
+    loadTwitterWidgets()
+  }
+
   render() {
     const unwraped = [{path: this.props.params.id + '.md'}];
 
